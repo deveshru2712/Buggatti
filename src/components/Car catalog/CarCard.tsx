db7fbec546e942b92f96edc0793ad052b8bafe9b
Devesh Chandra <deveshru2712@gmail.com>
2025-08-03 22:15:21 +0530

    added cars collection section

--- src/components/Car catalog/CarCard.tsx
+++ src/components/Car catalog/CarCard.tsx
@@ -1,25 +1,53 @@
-import { title } from "process";
-
 interface CarCardProps {
   text: string;
-  overlayText: string;
+  insignia: string;
   image: string;
   subText: string;
+  video?: string;
 }
 
-const CarCard = ({ text, overlayText, image, subText }: CarCardProps) => {
+const CarCard = ({ text, video, insignia, image, subText }: CarCardProps) => {
   return (
-    <div className="relative h-full w-full">
-      <div className="font-family-mono absolute flex flex-col items-center justify-between">
-        <div className="flex flex-col gap-2">
-          <h2>{text.toUpperCase()}</h2>
-          <img src={overlayText} alt={title} />
-        </div>
-        <div className="text-sm underline underline-offset-4 hover:underline-offset-2">
-          {subText.toUpperCase()}
+    <div className="group relative h-full w-full cursor-pointer overflow-hidden rounded-md">
+      <div className="absolute top-1/4 z-10 w-full -translate-y-3/4">
+        <img
+          src={insignia}
+          alt={text}
+          className="mx-auto w-2/3"
+          style={{ filter: "invert(1) brightness(2)" }}
+        />
+      </div>
+
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-between py-8">
+        <div className="flex h-full w-full flex-col items-center justify-between gap-2">
+          <h2 className="text-sm">{text.toUpperCase()}</h2>
+          <div className="cursor-pointer text-sm underline underline-offset-4 transition-all duration-300 hover:underline-offset-2">
+            {subText.toUpperCase()}
+          </div>
         </div>
       </div>
-      <img src={image} alt={title} />
+
+      {video && (
+        <div className="absolute inset-0 z-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          <video
+            src={video}
+            loop
+            muted
+            autoPlay
+            playsInline
+            preload="auto"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      )}
+
+      <img
+        src={image}
+        alt={text}
+        className={`h-full w-full object-cover transition-opacity duration-300 ${
+          video ? "opacity-100 group-hover:opacity-0" : "opacity-100"
+        }`}
+      />
     </div>
   );
 };
--- src/components/Car catalog/CarSection.tsx
+++ src/components/Car catalog/CarSection.tsx
@@ -1,11 +1,31 @@
+import { Cars } from "../../constants";
+import CarCard from "./CarCard";
+import { motion } from "motion/react";
+
 const CarSection = () => {
   return (
-    <section className="space-y-8 p-6 md:px-16">
+    <motion.section
+      initial={{ filter: "blur(20px)" }}
+      whileInView={{ filter: "blur(0px)" }}
+      viewport={{ once: true, margin: "0px 0px -100px 0px" }}
+      className="space-y-12 p-6 md:px-16"
+    >
       <h2 className="font-family-display text-3xl text-white md:text-5xl">
         OUR HYPER SPORTS CARS
       </h2>
-      <div className="grid w-full grid-cols-1 items-center gap-5 rounded-md border bg-gradient-to-b from-[#181818] to-black px-8 py-6 text-white md:grid-cols-2"></div>
-    </section>
+      <div className="grid h-full w-full grid-cols-1 items-center gap-10 rounded-md border border-white/20 bg-gradient-to-b from-[#181818] to-black px-6 py-8 text-white md:grid-cols-2 md:px-12 md:py-12 lg:px-36">
+        {Cars.map((car) => (
+          <CarCard
+            key={car.id}
+            image={car.image}
+            insignia={car.insignia}
+            subText={car.subText}
+            text={car.text}
+            video={car.video}
+          />
+        ))}
+      </div>
+    </motion.section>
   );
 };
 
